c183be4b969c369074dd6b9d3ff2b63b2ab89d07
Andrew Deck <andrew.deck@triblio.com>
2019-12-02 21:48:54 -0500

    day 2 part 2

--- 2019/day2/intcode.js
+++ 2019/day2/intcode.js
@@ -4,16 +4,32 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 
 
-partOne();
+// partOne();
+partTwo();
+
+function partTwo() {
+  let noun, verb;
+
+  loop:
+  for(noun = 0; noun < 100; noun++) {
+    for(verb = 0; verb < 100; verb++) {
+      let result = partOne(noun, verb);
+      if(result === 19690720) {
+        console.info(`found noun: ${noun} verb: ${verb}`);
+        break loop;
+      }
+    }
+  }
+  console.info(100 * noun + verb);
+}
 
 
-
-function partOne() {
+function partOne(noun, verb) {
   let program = input.split(',').map(x => Number(x));
 
   // overrides
-  program[1] = 12;
-  program[2] = 2;
+  program[1] = noun;
+  program[2] = verb;
 
   let cur = 0;
 
@@ -35,5 +51,5 @@ function partOne() {
     cur += 4;
   }
   
-  console.info(program[0]);
+  return program[0];
 }
